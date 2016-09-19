7a58b2e8cf9f8b189453ec8f94ac62e33c3151bd
danielpierre <daniel.l.pierre@gmail.com>
2016-09-19 04:01:15 -0400

    Added card randomizer

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,111 @@
 
 // CHARADES CARDS ---------------------
 
+var cards = [
+	[
+		"fatal error",
+		"nesting",
+		"strong",
+		"z-index",
+		"focus",
+		"closure",
+		"API key",
+		"Ajax call",
+		"list item",
+		"search and replace",
+		"ladies learning code",
+		"fishbowl",
+		"touch",
+		"react",
+		"blockquote",
+		":before",
+		":after",
+		"hover",
+		"wrapper",
+		"push",
+		"handlebars",
+		"link",
+		"scope",
+		"extends",
+		"header",
+		"footer",
+		"body",
+		"fetch",
+		"gulp watch",
+		"padding",
+		"sick!",
+		"slice",
+		"flexbox",
+		"flex-wrap",
+		"float",
+		"loop",
+		"child element",
+		"parent element",
+		"pizza"
+	], // [0]
+	[
+		"curly braces",
+		"hero image",
+		"font awesome",
+		"promise",
+		"add class",
+		"embed",
+		"deploy",
+		"help cue",
+		"smooth scroll",
+		"form",
+		"SASS",
+		"mixins",
+		"closure",
+		"ghost space",
+		"callback function",
+		"marquee",
+		"fat arrow",
+		"responsive",
+		"position: fixed",
+		"clearfix",
+		"fade in",
+		"important!",
+		"Simon Bloom",
+		"event listener",
+		"JavaScript",
+		"master branch",
+		"filter",
+		"flickity",
+		"canvas",
+		"calc()",
+		"sibling element",
+		"box sizing",
+		"carousel",
+		"background image",
+		"Salad Clurb"
+	], // [1]
+	[
+		"margin 0 auto",
+		"internet explorer",
+		"terminal",
+		"Homeward Bound",
+		"span",
+		"repeater field",
+		"concatenate",
+		"git init",
+		"git push master origin",
+		"dank meme",
+		"if...else statement",
+		"jQuery",
+		"masonry",
+		"firebase",
+		"bahn mi boys",
+		"Sylvia Nguyen",
+		"Ryan Christiani",
+		"nth-child",
+		"div",
+		"Red Hot Chili Peppers",
+		"Jack Nicholson in 1974"
+	] // [2]
+];
+
+/*
 var round1 = [
 	"fatal error",
 	"nesting",
@@ -77,7 +182,7 @@ var round1 = [
 	"child element",
 	"parent element",
 	"pizza"
-]
+];
 
 var round2 = [
 	"curly braces",
@@ -115,7 +220,7 @@ var round2 = [
 	"carousel",
 	"background image",
 	"Salad Clurb"
-]
+];
 
 var round3 = [
 	"margin 0 auto",
@@ -139,12 +244,28 @@ var round3 = [
 	"div",
 	"Red Hot Chili Peppers",
 	"Jack Nicholson in 1974"
-]
+];
+*/
 
+// console.log('round1', cards[0]);
+// console.log('round2', cards[1]);
+// console.log('round3', cards[2]);
+
+// console.log('Shuffled');
+
+// cards[0] = _.shuffle(cards[0]);
+// cards[1] = _.shuffle(cards[1]);
+// cards[2] = _.shuffle(cards[2]);
+
+// console.log('round1', cards[0]);
+// console.log('round2', cards[1]);
+// console.log('round3', cards[2]);
 
 
 // Arrive on page and the avatar screen is displayed with instructions
-
+var round = 1; // initialize round counter
+var points = [0, 0];
+var firstCard = true;
 
 // User clicks start button
 // Screen fades to play mode
@@ -172,9 +293,44 @@ var startGame = function(){
 			//when time is up stop countdown and empty div
 			window.clearInterval(countdown);
 			$('.timer').html('');
+			round++; // go to next round
+			firstCard = true;
 		}seconds--;//decrease seconds by 1
 
 	},1000);
+
+	nextCard(round, cards);
+}
+
+function nextCard(round, cards) {
+	var deck;
+
+	if (firstCard === true) {
+		deck = _.shuffle(cards[round-1]);
+		firstCard = false;
+	} else {
+		deck = cards;
+	}
+	console.log('deck', deck);
+	console.log('length', deck.length);
+	console.log('points', points);
+
+	$('.charades-card').html(deck.pop());
+
+	$(window).keypress(function(e) {
+	    if (e.which === 32) {
+	    	points[0]++;
+	    	nextCard(round, deck);
+	    }
+	    $(this).off(e);
+	});
+
+	$('.next-button').click(function(e) {
+		points[0]++;
+		nextCard(round, deck);
+		$(this).off(e);
+	});
+
 }
 // Countdown clock begins
 // Every time user hits space bar a new charades card appears and a point is added to that team's total
@@ -195,9 +351,9 @@ var startGame = function(){
 
 
 
-$(window).keypress(function(e) {
-    if (e.which === 32) {
+// $(window).keypress(function(e) {
+//     if (e.which === 32) {
 
 
-    }
-});
+//     }
+// });
